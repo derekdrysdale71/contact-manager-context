74ea87187c955aeedf6918cbb79e50662f217391
Derek <derek@darthvader.local>
2018-08-21 16:20:16 -0700

    move state to context

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,29 +1,8 @@
 import React, { Component } from 'react'
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '555-555-4444'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '555-555-3333'
-      }
-    ]
-  };
 
   removeContact = id => {
     const { contacts } = this.state;
@@ -32,17 +11,23 @@ class Contacts extends Component {
   }
 
   render() {
-    const { contacts } = this.state;
     return (
-      <React.Fragment>
-        {contacts.map(contact =>
-          <Contact
-            key={contact.id}
-            contact={contact}
-            removeContact={this.removeContact.bind(this, contact.id)}
-          />
-        )}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact =>
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  removeContact={this.removeContact.bind(this, contact.id)}
+                />
+              )}
+            </React.Fragment>
+          )
+        }}
+      </Consumer>
     )
   }
 }
